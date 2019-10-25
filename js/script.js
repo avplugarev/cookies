3c8dc77d269b9950df4112863e237161d0eb857d
Nightgust <nightgust@ALEXANDERs-MacBook-Pro.local>
2019-10-26 02:13:58 +0300

    add set cooky

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-const save_btn = document.querySelector('save');
-const clear_btn = document.querySelector('clear');
+const save_btn = document.querySelector('#save');
+const clear_btn = document.querySelector('#clear');
+const input_area = document.querySelector('#area');
+
+function setCook (name, value) {
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+    document.cookie = updatedCookie;
+}
+
+save_btn.onclick = () => {
+    setCook("bravo", input_area.value);
+    console.log("поставил");
+}
