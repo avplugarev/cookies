6c0b906f656e57eb9f902e47b61f276bbf207f49
Nightgust <nightgust@ALEXANDERs-MacBook-Pro.local>
2019-10-26 20:41:14 +0300

    refacror js save_choice: add disabled before reload and block checkboxes

--- js/save_choices.js
+++ js/save_choices.js
@@ -1,18 +1,25 @@
 const sabmit_btn = document.querySelector('#submit');
-let preferences = document.getElementsByClassName('preferences')
+let preferences = document.getElementsByClassName('preferences');
+
+//функция проверки послде загрузки сделан ли уже выбор
+function check() {
+    for (i=0;i<preferences.length;i++) {
+        preferences[i].checked = (localStorage.getItem(preferences[i].id + ".checked") === "true");
+        preferences[i].disabled = localStorage.getItem(preferences[i].id + ".disabled");
+    };
+    localStorage.getItem(preferences[0].id + ".checked") != null ? sabmit_btn.disabled = "true" : undefined;
+}
 
 //сохраняем значение в локал сторадж
 sabmit_btn.onclick = () => {
     for (i=0;i<preferences.length;i++) {
         localStorage.setItem(preferences[i].id+".checked", preferences[i].checked);
-        localStorage.setItem(preferences[i].id+".disabled", true);
+        localStorage.setItem(preferences[i].id+".disabled", "true");
+        preferences[i].disabled = true;
         }
     sabmit_btn.disabled = true;
     };
 //если есть сохраненные данные - вытаскиваем их из локал сторадж
-for (i=0;i<preferences.length;i++) {
-    preferences[i].checked = (localStorage.getItem(preferences[i].id + ".checked") === "true");
-    preferences[i].disabled = localStorage.getItem(preferences[i].id + ".disabled", true);
-  };
+check();
 
 
