7d6b7e3b2f13f14399bd7111a5a9712b6227a9c8
Nightgust <nightgust@ALEXANDERs-MacBook-Pro.local>
2019-10-26 15:55:47 +0300

    add function show and hide

--- js/save_city.js
+++ js/save_city.js
@@ -34,33 +34,41 @@ function getCookie(name) {
   ));
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
+
+//функция скрытия и появление кнопок, текста, блоков
+function hideAndShow (saveVisible, clearVisible, messageVisible, inputVisible, resultVisible) {
+    save_btn.style.display = saveVisible;
+    clear_btn.style.display = clearVisible;
+    message.style.display = messageVisible;
+    input_area.style.display = inputVisible;
+    result.style.display = resultVisible;
+}
+
+//обработчик кнопки сохранить
+save_btn.onclick = () => {
+    setCookies("location", input_area.value);
+    hideAndShow('none','none', 'none', 'none');
+    result.innerText = "Спасибо. Ваши данные сохранены.";
+}
+
+//обработчик кнопки очистить
+clear_btn.onclick = () => {
+    deleteCookies("location");
+    hideAndShow('none','none', 'none', 'none');
+    result.innerText = "Спасибо. Ваши данные очищены.";
+}
+
 //определяем при заходе стоит ли уже кука
 let city = getCookie('location');
 
-//если стоит - скрываем вопрос
+//если не стоит - показываем вопрос
 if (city == undefined) {
+    hideAndShow('inline','none');
     message.innerText="Введите ваш город пребывания:"
-    save_btn.style.display = 'inline';
-    clear_btn.style.display = 'none';
-}
+} //иначе скрываем вопрос и показываем город
 else {
+        hideAndShow('none','inline', 'block', 'none');
         message.innerHTML="Ваш город: '"+(city)+"'";
         input_area.style.display='none'
-        save_btn.style.display='none';
-        clear_btn.style.display = 'inline';
     }
 
-save_btn.onclick = () => {
-    setCookies("location", input_area.value);
-    result.innerText = "Спасибо. Ваши данные сохранены.";
-    save_btn.style.display = 'none';
-    input_area.style.display = 'none';
-    message.style.display = 'none';
-}
-
-clear_btn.onclick = () => {
-    deleteCookies("location");
-    message.style.display = 'none';
-    result.innerText = "Спасибо. Ваши данные очищены.";
-    clear_btn.style.display = 'none';
-}
