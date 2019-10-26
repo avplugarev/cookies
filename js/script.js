895aa7aa1ce174a2f662c81d3c813882133d2e7c
Nightgust <nightgust@ALEXANDERs-MacBook-Pro.local>
2019-10-26 03:44:21 +0300

    small cosmetic changes in first task

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const save_btn = document.querySelector('#save');
 const clear_btn = document.querySelector('#clear');
 const input_area = document.querySelector('#area');
+const myquery = document.querySelector('#query');
+const myCity = document.querySelector('#myCity');
+const message = document.querySelector('#message');
+const result = document.querySelector('#result');
+message.style.display = 'none';
+myquery.style.display = 'none';
+input_area.style.display = 'none';
+
+
+
 
 //функция установки куки
 function setCookies (name, value, options = {}) {
@@ -25,13 +35,45 @@ function deleteCookies(name) {
     document.cookie = name+"=user"+"domain=localhost; path=/; max-age=-1";
 }
 
+//функция получить куку
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+//определяем при заходе стоит ли уже кука
+let city = getCookie('location');
+
+//если стоит - скрываем вопрос
+if (city == undefined) {
+    myquery.style.display = 'block';
+    input_area.style.display = 'block';
+    save_btn.style.display = 'inline';
+    clear_btn.style.display = 'none';
+}
+else {
+        console.log("dfdfd")
+        message.style.display = 'block';
+        myCity.innerText=city;
+        save_btn.style.display='none';
+        clear_btn.style.display = 'inline';
+        console.log(city);
+    }
 
 save_btn.onclick = () => {
     setCookies("location", input_area.value);
+    result.innerText = "Спасибо. Ваши данные сохранены.";
+    save_btn.style.display = 'none';
+    input_area.style.display = 'none';
+    myquery.style.display = 'none';
     console.log("поставил");
 }
 
 clear_btn.onclick = () => {
     deleteCookies("location");
+    message.style.display = 'none';
+    result.innerText = "Спасибо. Ваши данные очищены.";
+    clear_btn.style.display = 'none';
     console.log("удалил");
 }
